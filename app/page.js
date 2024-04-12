4127fa139e8229fb9acd85f71ffc7d101c77f528
Piyush-Sharma79 <g2001idseptember@gmail.com>
2024-04-12 22:58:26 +0530

    f

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
           </nav>
           <div className='text-center p-10 '>
             <h2 className='text-5xl py-2 text-teal-600 dark:text-teal-400 font-medium md:text-6xl'>Piyush</h2>
-            <h3 className='text-2xl py-2 dark:text-white'>Developer</h3>
+            <h3 className='text-2xl py-2 dark:text-white'>Full Stack Developer</h3>
             <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white'>
             I code beautifully simple things, and I love what I do.
             </p>
